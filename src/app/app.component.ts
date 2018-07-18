862cd0618d3e1fefd375b7390b3965fde31fe29f
Alp Bilgin <alp_bilgin@hotmail.com>
2018-07-18 14:42:06 +0200

    sekil verme cabalari

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { OyunTahtasi } from './oyuntahtasi';
+import { style } from 'typestyle';
+
+const gosterge = style({
+  outline: 'black 1px solid'
+});
+
+const yurume = style({
+  outline: 'blue 1px solid'
+});
+
+const icerik = style({
+  position: 'absolute'
+});
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div *ngFor="let satir of oyuntahtasi.yerler; let i = index" class="row">
+      <div *ngFor="let yer of satir; let j = index" [class.${gosterge}]="yer.getHighlight()" [class.${yurume}]="yer.yurumeHighlight"
+      class="col" (click)="yerTiklama(i,j)">
+        <div class="${icerik}">{{ yer.getTerrain().getResim() }}</div>
+        <div class="${icerik}">{{ yer.getTash().getResim() }}</div>
+      </div>
+    </div>
+  `,
+  styles: ['.row { height: 100px; }']
 })
 export class AppComponent implements OnInit {
   title = 'app';
