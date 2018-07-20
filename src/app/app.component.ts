874bdc87131ef570fba6d87fa37d71fa6eb3b9c6
Alp Bilgin <alp_bilgin@hotmail.com>
2018-07-20 13:26:28 +0200

    angular2-inline ve tempalte refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { OyunTahtasi } from './oyuntahtasi';
 import { style } from 'typestyle';
 
-const gosterge = style({
-  outline: 'black 1px solid'
-});
-
-const yurume = style({
-  outline: 'blue 1px solid'
-});
-
-const icerik = style({
-  position: 'absolute'
-});
-
 @Component({
   selector: 'app-root',
   template: `
     <div *ngFor="let satir of oyuntahtasi.yerler; let i = index" class="row">
-      <div *ngFor="let yer of satir; let j = index" [class.${gosterge}]="yer.getHighlight()" [class.${yurume}]="yer.yurumeHighlight"
-      class="col" (click)="yerTiklama(i,j)">
-        <div class="${icerik}">{{ yer.getTerrain().getResim() }}</div>
-        <div class="${icerik}">{{ yer.getTash().getResim() }}</div>
+      <div *ngFor="let yer of satir; let j = index"
+      class="col {{highlightStyle(yer)}}"
+      (click)="yerTiklama(i,j)">
+        <div class="{{icerik}}">{{ yer.getTerrain().getResim() }}</div>
+        <div class="{{icerik}}">{{ yer.getTash().getResim() }}</div>
       </div>
     </div>
   `,
   styles: ['.row { height: 100px; }']
 })
 export class AppComponent implements OnInit {
+
+  gosterge = style({ outline: 'black 1px solid' });
+
+  yurume = style({ outline: 'blue 1px solid' });
+
+  icerik = style({ position: 'absolute' });
+
   title = 'app';
   oyuntahtasi: OyunTahtasi;
 
@@ -39,6 +35,14 @@ export class AppComponent implements OnInit {
     this.oyuntahtasi.secimleriTemizle();
     this.oyuntahtasi.seciliyer = { i: i, j: j };
     this.oyuntahtasi.seciliyerİsaretle();
+  }
+
+  highlightStyle(yer) {
+    if (yer.getHighlight()) {
+      return this.gosterge.toString();
+    } else if (yer.yurumeHighlight) {
+      return this.yurume.toString();
+    }
   }
 
 }
