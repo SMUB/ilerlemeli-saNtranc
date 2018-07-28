fb61aa304f9640080fb71d4a6a44c490517cea8d
Alp Bilgin <alp_bilgin@hotmail.com>
2018-07-28 19:48:10 +0200

    Taş seti tamamlandı.

--- src/app/oyuntahtasi.ts
+++ src/app/oyuntahtasi.ts
@@ -1,5 +1,5 @@
 import { Yer } from './yer';
-import { Tash } from './tash';
+import { Tash, At, AltPiyon, Fil, Kale, Vezir, ÜstPiyon, Şah } from './tash';
 import { Terrain } from './terrain';
 
 class SeciliYer {
@@ -16,7 +16,32 @@ export class OyunTahtasi {
         for (let i = 0; i < x; i++) {
             this.yerler.push(new Array<Yer>());
             for (let j = 0; j < y; j++) {
-                const tash = new Tash(' ♘ ');
+                let tash = new Tash('');
+                if (i === 1) {
+                    tash = new ÜstPiyon(' ♙ ');
+                } else if (i === 6) {
+                    tash = new AltPiyon(' ♟ ');
+                } else if (i === 0 && (j === 0 || j === 7)) {
+                    tash = new Kale(' ♖ ');
+                } else if (i === 7 && (j === 0 || j === 7)) {
+                    tash = new Kale(' ♜ ');
+                } else if (i === 0 && (j === 1 || j === 6)) {
+                    tash = new At(' ♘ ');
+                } else if (i === 7 && (j === 1 || j === 6)) {
+                    tash = new At(' ♞ ');
+                } else if (i === 0 && (j === 2 || j === 5)) {
+                    tash = new Fil(' ♗ ');
+                } else if (i === 7 && (j === 2 || j === 5)) {
+                    tash = new Fil(' ♝ ');
+                } else if (i === 0 && j === 3) {
+                    tash = new Vezir(' ♔ ');
+                } else if (i === 7 && j === 3) {
+                    tash = new Şah(' ♛ ');
+                } else if (i === 0 && j === 4) {
+                    tash = new Şah(' ♕ ');
+                } else if (i === 7 && j === 4) {
+                    tash = new Vezir(' ♚ ');
+                }
                 const terrain = new Terrain('|__|');
                 this.yerler[i].push(new Yer(tash, terrain));
             }
@@ -28,8 +53,10 @@ export class OyunTahtasi {
         seciliYer.setHighlight(true);
         const tash = seciliYer.getTash();
         if (tash) {
-            const hamleler = tash.getHamleler()[0];
-            this.yerler[this._seciliyer.i + hamleler.i][this._seciliyer.j + hamleler.j].yurumeHighlight = true;
+            const hamleler = tash.getHamleler();
+            for (const hamle of hamleler) {
+                this.yerler[this._seciliyer.i + hamle.i][this._seciliyer.j + hamle.j].yurumeHighlight = true;
+            }
         }
     }
 
--- src/app/tash.ts
+++ src/app/tash.ts
@@ -1,6 +1,10 @@
+export enum hamlecinsi { yürüme, yeme, yiyerekyürüme }
+
 class Hamle {
     public i: number;
     public j: number;
+    public cins: hamlecinsi;
+    public tekrar: number;
 }
 
 // TODO farklı cinste taşları alt class olarak tanımla.
@@ -8,11 +12,10 @@ class Hamle {
 
 export class Tash {
     private resim: string;
-    private hamleler: Hamle[];
+    protected hamleler: Hamle[];
 
     constructor(resim: string) {
         this.resim = resim;
-        this.hamleler = [{ i: 1, j: 0 } as Hamle];
     }
 
     getResim() {
@@ -21,5 +24,99 @@ export class Tash {
 
     getHamleler() {
         return this.hamleler;
+    }
+}
+
+export class At extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: 2, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 2, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -2, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -2, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 1, j: 2, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 1, j: -2, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -1, j: 2, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -1, j: -2, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle
+        ];
+    }
+}
+
+export class AltPiyon extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: -1, j: 0, cins: hamlecinsi.yürüme, tekrar: 1 } as Hamle,
+            { i: -1, j: 1, cins: hamlecinsi.yeme, tekrar: 1 } as Hamle,
+            { i: -1, j: -1, cins: hamlecinsi.yeme, tekrar: 1 } as Hamle
+        ];
+    }
+}
+
+export class ÜstPiyon extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: 1, j: 0, cins: hamlecinsi.yürüme, tekrar: 1 } as Hamle,
+            { i: 1, j: 1, cins: hamlecinsi.yeme, tekrar: 1 } as Hamle,
+            { i: 1, j: -1, cins: hamlecinsi.yeme, tekrar: 1 } as Hamle
+        ];
+    }
+}
+
+export class Fil extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: 1, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: -1, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: -1, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 1, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle
+        ];
+    }
+}
+
+export class Kale extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: 1, j: 0, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: -1, j: 0, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 0, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 0, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle
+        ];
+    }
+}
+
+export class Vezir extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: 1, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 1, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: -1, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: -1, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 1, j: 0, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: -1, j: 0, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 0, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle,
+            { i: 0, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: Infinity } as Hamle
+        ];
+    }
+}
+
+export class Şah extends Tash {
+    constructor(resim: string) {
+        super(resim);
+        this.hamleler = [
+            { i: 1, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 1, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -1, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -1, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 1, j: 0, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: -1, j: 0, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 0, j: 1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle,
+            { i: 0, j: -1, cins: hamlecinsi.yiyerekyürüme, tekrar: 1 } as Hamle
+        ];
     }
 }
