0671acad4d5e846e7f50b74b3390613acb695014
Alp Bilgin <alp_bilgin@hotmail.com>
2019-06-23 22:12:51 +0200

    design better tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -55,11 +55,11 @@ describe('Game mechanics', () => {
     // Inject test layout 
     app.oyuntahtasi.piecesService.pieceSequence = [
       [0, 0, 0, 0, 0, 0, 0, 0],
-      [-2, -3, -4, 0, 0, 0, 0, 0],
-      [0, 0, 0, 1, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0, 0, 0, 0],
-      [1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, -4, 0, 0, 0, 0, -3],
       [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 1, 0, 0],
+      [0, 0, 0, 0, -2, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 1]
     ];
@@ -93,27 +93,46 @@ describe('Game mechanics', () => {
   it('should move zombie bishop when human pawn moves', async(() => {
     const compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
-    expect(compiled.querySelector('#C63')).toBeDefined();
-    // select pawn
-    app.yerTiklama(63);
+    expect(compiled.querySelector('#C27')).toBeDefined();
+    // select sacrifical pawn
+    app.yerTiklama(27);
     // move pawn one forward
-    app.yerTiklama(55);
-    // re render
+    app.yerTiklama(19);
+    // re render and check if he is consumed by bishop
     fixture.detectChanges();
-    for (let i = 0; i < 64; i++) {
-      if (i === 19) { // bishop replaced bait pawn
-        let pieceContentOfCell = compiled.querySelector(`#C${i}`).querySelector('.piece');
-        let fileName = pieceContentOfCell.attributes.getNamedItem('src').value;
-        expect(fileName).toEqual('./assets/-4.png');
-      } else if (i === 55) { // manipulation pawn should be in new position
-        let pieceContentOfCell = compiled.querySelector(`#C${i}`).querySelector('.piece');
-        let fileName = pieceContentOfCell.attributes.getNamedItem('src').value;
-        expect(fileName).toEqual('./assets/1.png');
-      } else if (i === 63) { // manipulation pawn should not be in
-        let pieceContentOfCell = compiled.querySelector(`#C${i}`).querySelector('.piece');
-        expect(pieceContentOfCell).toBeNull();
-      }
-
-    }
+    let pieceContentOfCell = compiled.querySelector(`#C19`).querySelector('.piece');
+    let fileName = pieceContentOfCell.attributes.getNamedItem('src').value;
+    expect(fileName).toEqual('./assets/-4.png');
   }));
+
+  it('should move zombie knight when human pawn moves', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+    expect(compiled.querySelector('#C29')).toBeDefined();
+    // select sacrifical pawn
+    app.yerTiklama(29);
+    // move pawn one forward
+    app.yerTiklama(21);
+    // re render and check if he is consumed by knight
+    fixture.detectChanges();
+    let pieceContentOfCell = compiled.querySelector(`#C21`).querySelector('.piece');
+    let fileName = pieceContentOfCell.attributes.getNamedItem('src').value;
+    expect(fileName).toEqual('./assets/-3.png');
+  }));
+
+  it('should move zombie rook when human pawn moves', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+    expect(compiled.querySelector('#C29')).toBeDefined();
+    // select sacrifical pawn
+    app.yerTiklama(40);
+    // move pawn one forward
+    app.yerTiklama(32);
+    // re render and check if he is consumed by rook
+    fixture.detectChanges();
+    let pieceContentOfCell = compiled.querySelector(`#C32`).querySelector('.piece');
+    let fileName = pieceContentOfCell.attributes.getNamedItem('src').value;
+    expect(fileName).toEqual('./assets/-2.png');
+  }));
+
 });
